a414494d3a72c3f6c12a7cad16b887c2a569dc0a
yeti-detective <yeti.detective@gmail.com>
2018-11-07 22:24:09 -0500

    adding test coverage

---
+++ tweetsToCSV.test.js
@@ -0,0 +1,7 @@
+const createContent = require("./tweetsToCSV").createContent;
+
+// TODO: mock originalTweetsNoMentions object & rewrite code so methods can be
+// tested before all data is processed
+test("createContent concatenates input to csv formatted text fields", () => {
+  expect(true).toBeTrue();
+});
