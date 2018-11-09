c6d9b81e46d167737c3a72af03ad190cd39a48a1
yeti-detective <yeti.detective@gmail.com>
2018-11-09 16:39:52 -0500

    first test setup & red

--- filterTweets.test.js
+++ filterTweets.test.js
@@ -1,5 +1,16 @@
-const createContent = require("./tweetsToCSV").createContent;
+const filterTweets = require("./filterTweets");
+
+const createContent = filterTweets.createContent;
+const originalTweets = filterTweets.originalTweetsNoMentions;
 
 test("createContent concatenates input to csv formatted text fields", () => {
-  expect(true).toBe(true);
+  // Arrange
+  const tweet1 = { full_text: "full text of tweet 1" };
+  const tweet2 = { full_text: "full text of tweet 2" };
+  const expectedTweetsInput = [tweet1, tweet2];
+  const expectedString = `${tweet1.full_text}, ${tweet2.full_text}\n`;
+  // Act
+  const actualString = createContent(expectedTweetsInput);
+  // Assert
+  expect(expectedString).toMatch(RegExp(actualString));
 });
