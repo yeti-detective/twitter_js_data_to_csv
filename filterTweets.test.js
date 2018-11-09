a6ed494ede5be423bea92063b274818765c14b71
yeti-detective <yeti.detective@gmail.com>
2018-11-09 12:32:23 -0500

    appropriately rename test file

--- filterTweets.test.js
+++ filterTweets.test.js
@@ -1,7 +1,5 @@
 const createContent = require("./tweetsToCSV").createContent;
 
-// TODO: mock originalTweetsNoMentions object & rewrite code so methods can be
-// tested before all data is processed
 test("createContent concatenates input to csv formatted text fields", () => {
-  expect(true).toBeTrue();
+  expect(true).toBe(true);
 });
