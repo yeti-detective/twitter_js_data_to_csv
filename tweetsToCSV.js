ea7ead0828bc1d85f1380478c82bb748a9e4685c
yeti-detective <yeti.detective@gmail.com>
2018-11-10 22:23:00 -0500

    removing unnecessary console log

--- tweetsToCSV.js
+++ tweetsToCSV.js
@@ -5,7 +5,7 @@ const originalTweets = require("./filterTweets").originalTweetsNoMentions;
 const createContent = require("./filterTweets").createContent;
 
 const file = "tweets.csv";
-console.log("are you even getting to line six, dawg?");
+
 const tweetScript = process.env.TESTTWEET || require("./twitter_stuff/tweet");
 
 fs.writeFile(file, createContent(originalTweets), err => {
